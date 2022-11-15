37d4cc0fc90c16675adad37af54e397e0a79f63f
BuzinAlexey <bess.post@mail.ru>
2022-11-15 14:15:48 +0300

    End work DZ1

--- DZ1/script2.js
+++ DZ1/script2.js
@@ -1,12 +1,14 @@
 //Задание 1
-
+"use strict";
 function temp() {
-  let t = document.getElementById("t").value;
+  let t = Number.parseFloat(document.getElementById("t").value);
   let c = (t - 32) * (5/9);
+  c = Math.round(c);
   alert(`${t} F° -> ${c} C°`);
   }
-  function temp1() {
-    let t = document.getElementById("t1").value;
+function temp1() {
+    let t = Number.parseFloat(document.getElementById("t1").value);
     let f = (t) * (9/5)+32;
+    f = Math.round(f);
     alert(`${t} C° -> ${f} F°`);
   }
